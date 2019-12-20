41ddf8100d88b52c399016873e0e4667b0bcdb57
371243 <371243@PC388739.cts.com>
2019-12-20 18:48:35 +0530

    cors issue

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,7 @@ public class EmployeeController {
 	
 	
 	@PostMapping(value="/addAlien", consumes=MediaType.APPLICATION_JSON_VALUE)
+	//@CrossOrigin("http:://localhost:3000")
 	public ResponseEntity<Employee> addAlien(@RequestBody Employee employee){
 		Employee emp = repo.save(employee);
 		return new ResponseEntity<Employee>(emp, HttpStatus.OK);
